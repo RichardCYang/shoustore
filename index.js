89f6f7ed3decc504c13bad2f00b8dcff702a59ba
MrDB <bnuik77212@gmail.com>
2022-04-28 12:19:32 +0900

    서버 메인 파일관리 기능 분리 By 양찬영_220428

--- index.js
+++ index.js
@@ -4,15 +4,12 @@ conlog.coninfo('Server started...');
 conlog.coninfo('Importing modules...');
 
 const http = require('http');
-const fs = require('fs');
 const ws = require('ws');
 const dbmng = require('./server/dbmanager');
-const sessionmng = require('./server/sessionmanager');
-const filemanager = require('./server/filemanager');
+const filemng = require('./server/filemanager');
 
 conlog.confin('Importing modules');
 
-const hostname = '127.0.0.1';
 const port = 8090;
 
 conlog.coninfo('Initializing database...');
@@ -48,73 +45,11 @@ function parseWSData( data ){
     return arr;
 }
 
-function removeRootNodeDir( filelist,rootdir ){
-    let newArr = [];
-    for( let i = 0; i < filelist.length; i++ ){
-        let pos = filelist[i].indexOf( rootdir );
-        newArr.push( filelist[i].substr( pos + rootdir.length ) );
-    }
-    return newArr;
-}
-
-function readdirSyncRecursive( path ){
-    let filesArr = [];
-    try{
-        let files = fs.readdirSync( path );
-        for(let i = 0; i < files.length; i++){
-            let lstat = fs.lstatSync( path + '/' + files[i]);
-            if( lstat.isDirectory() ){
-                let subfiles = readdirSyncRecursive( path + '/' + files[i] );
-                filesArr = filesArr.concat( subfiles );
-            }else{
-                filesArr.push( path + '/' + files[i] );
-            }
-        }
-    }catch(err){
-        console.log(err);
-    }
-    return filesArr;
-}
-
-function getRealmPath( path ){
-    let clientFiles = [];
-    clientFiles = readdirSyncRecursive('./client');
-    clientFiles = removeRootNodeDir(clientFiles,'client');
-
-    if( clientFiles.includes(path) ){
-        return './client';
-    }else{
-        return './server';
-    }
-}
-
-function loadResource( res,path ){
-    if( path == '/' ){
-        path = '/index.html';
-    }
-    if( path.indexOf("?") > -1 ){
-        path = path.split("?")[0];
-    }
-    try{
-        let rawData;
-        if( path.endsWith('.svg') ){
-            res.writeHead(200,{'Content-Type':'image/svg+xml'});
-        }
-        if( path.endsWith('.css') ){
-            res.writeHead(200,{'Content-Type':'text/css'});
-        }
-        rawData = fs.readFileSync(getRealmPath( path ) + path);
-        res.end(rawData);
-    }catch(err){
-        console.log( err );
-    }
-}
-
 conlog.coninfo('Starting HTTP Internal Server...');
 
 const httpserver = http.createServer((req,res) => {
     if( req.method == 'GET' ){
-        loadResource( res,req.url );
+        filemng.loadResource( res,req.url );
     }else{
         res.writeHead(404,{'Content-Type':'text/plain'});
         res.write('This page not found 404 error');
--- server/filemanager.js
+++ server/filemanager.js
@@ -6,3 +6,65 @@ exports.loadImage = (imgname) => {
     let imgdata = fs.readFileSync( SERVERPATH_IMAGES + imgname,{encoding:'base64'} );
     return imgdata;
 }
+
+function removeRootNodeDir(filelist,rootdir){
+    let newArr = [];
+    for( let i = 0; i < filelist.length; i++ ){
+        let pos = filelist[i].indexOf( rootdir );
+        newArr.push( filelist[i].substr( pos + rootdir.length ) );
+    }
+    return newArr;
+}
+
+function readdirSyncRecursive(path){
+    let filesArr = [];
+    try{
+        let files = fs.readdirSync( path );
+        for(let i = 0; i < files.length; i++){
+            let lstat = fs.lstatSync( path + '/' + files[i]);
+            if( lstat.isDirectory() ){
+                let subfiles = readdirSyncRecursive( path + '/' + files[i] );
+                filesArr = filesArr.concat( subfiles );
+            }else{
+                filesArr.push( path + '/' + files[i] );
+            }
+        }
+    }catch(err){
+        console.log(err);
+    }
+    return filesArr;
+}
+
+function getRealmPath(path){
+    let clientFiles = [];
+    clientFiles = readdirSyncRecursive('./client');
+    clientFiles = removeRootNodeDir(clientFiles,'client');
+
+    if( clientFiles.includes(path) ){
+        return './client';
+    }else{
+        return './server';
+    }
+}
+
+exports.loadResource = (res,path) => {
+    if( path == '/' ){
+        path = '/index.html';
+    }
+    if( path.indexOf("?") > -1 ){
+        path = path.split("?")[0];
+    }
+    try{
+        let rawData;
+        if( path.endsWith('.svg') ){
+            res.writeHead(200,{'Content-Type':'image/svg+xml'});
+        }
+        if( path.endsWith('.css') ){
+            res.writeHead(200,{'Content-Type':'text/css'});
+        }
+        rawData = fs.readFileSync(getRealmPath( path ) + path);
+        res.end(rawData);
+    }catch(err){
+        console.log( err );
+    }
+}
