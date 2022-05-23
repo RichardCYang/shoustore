83d95eb3d03792ffba855aa94d5d4c9ad880ccd7
정진형 <j2hook@gmail.com>
2022-05-23 14:20:31 +0900

    상품 등록 db연결 by 정진형_220523

---
+++ server/actions/regitems.js
@@ -0,0 +1,14 @@
+
+const dbmng = require('../dbmanager');
+
+exports.regitems = ( ws,data ) => {
+    dbmng.addItem( data.name, data.category_name, data.price, data.stockcnt, data.thumbnail, data.itemdesc,(err) => {
+        if ( err ) {
+            console.log( err );
+            return;
+        }
+        ws.send( 'DONE_REGITEMS' );
+    }).catch((err) => {
+        console.log(err)
+    })
+}
