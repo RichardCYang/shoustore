933d34bd99da786a5f869df92e479f7b195f2fa7
Jinhyeong <j2hook@gmail.com>
2022-06-04 18:39:25 +0900

    regitemview.js 변경 by 정진형 220604

--- client/scripts/regitemview.js
+++ client/scripts/regitemview.js
@@ -13,5 +13,47 @@ function initCatsCombo(){
 }
 
 window.onload = () => {
+    document.setOnClickByID('regitemButton',() => {
+        let name = document.getElementById('nicknameInput');
+        let price = document.getElementById('priceInput');
+        let stockcnt = document.getElementById('countInput');
+        let category_name = document.getElementById('catSelect');
+        let itemdesc = document.getElementById('itemdescView');
+        let thumbnail = document.getElementById('thumbnailFile');
+
+        thumbnail.src = window.URL.createObjectURL(files[0]);
+
+        if( checkNullOrEmpty( name.value ) ){
+            window.parent.document.showMessageBox( '잘못된 입력','상품명을 입력해주세요!','error' )
+            return;
+        }
+        if( checkNullOrEmpty( price.value ) ){
+            window.parent.document.showMessageBox( '잘못된 입력','가격을 입력해주세요!','error' )
+            return;
+        }
+        if( checkNullOrEmpty( stockcnt.value ) ){
+            window.parent.document.showMessageBox( '잘못된 입력','재고를 입력해주세요!','error' )
+            return;
+        }
+        if( checkNullOrEmpty( category_name.value ) ){
+            window.parent.document.showMessageBox( '잘못된 입력','카테고리를 입력해주세요!','error' )
+            return;
+        }
+
+        wsc_simplesend('ac=regitems\n' + 'name=' + name.value + '\n' +
+                                         'category_name=' + category_name.value + '\n' +
+                                         'price=' + price.value + '\n' +
+                                         'stockcnt=' + stockcnt.value + '\n' +
+                                         'itemdesc=' + itemdesc.value + '\n' +
+                                         'thumbnail=' + thumbnail.src,(event) =>{
+        
+            if( event.data == "DONE_REGITEMS" ) {
+                window.parent.document.showMessageBox( '상품등록 성공','상품등록에 성공하였습니다!','info' );
+                location.href("../index.html");
+            } 
+                        
+        }) 
+
+    })
     initCatsCombo();
 }
